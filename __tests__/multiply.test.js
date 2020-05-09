75c84334658809337b24cbca59165d1c70a047e7
Anthony Ilinykh <anthonyilinykh@gmail.com>
2020-05-09 09:23:40 +0300

    redundant spaces removed

--- __tests__/multiply.test.js
+++ __tests__/multiply.test.js
@@ -1,16 +1,9 @@
-
-
 function multiply(a, b) {
 
 }
 
-
-
-
 test('multiply', () => {
-
   expect(multiply(2, 2)).toBe(4);
   expect(multiply(-1, 3)).toBe(-3);
   expect(multiply(10, 0.1)).toBe(1);
-
 });
