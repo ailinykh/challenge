4c419e66752d5d36d5d71bf39767286a500299f2
Anthony Ilinykh <anthonyilinykh@gmail.com>
2020-05-09 09:22:14 +0300

    first challenge

--- __tests__/game.test.js
+++
@@ -1,5 +0,0 @@
-const game = require("../game")
-
-test('game', function() {
-  expect(game()).toBe(undefined)  
-})
---
+++ __tests__/multiply.test.js
@@ -0,0 +1,16 @@
+
+
+function multiply(a, b) {
+
+}
+
+
+
+
+test('multiply', () => {
+
+  expect(multiply(2, 2)).toBe(4);
+  expect(multiply(-1, 3)).toBe(-3);
+  expect(multiply(10, 0.1)).toBe(1);
+
+});
